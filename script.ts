81ed3823130bf3b5c9fd4a82175dc5c5c96d8d06
unknown <342906989@TDSB.ON.CA>
2026-03-04 10:18:26 -0500

    Worked on canvas graph functionality

--- script.ts
+++ script.ts
@@ -1,6 +1,8 @@
+import type { ChartConfiguration } from 'chart.js/auto';
+
 const form = document.getElementById("cubic-form") as HTMLFormElement;
 const resultsContainer = document.getElementById("results-container") as HTMLElement;
-resultsContainer.style.visibility = "hidden";
+// resultsContainer.style.visibility = "hidden";
 
 // Displaying results
 function getCubicEquation(a: number, b: number, c: number, d: number): string {
@@ -39,36 +41,63 @@ function getCubicEquation(a: number, b: number, c: number, d: number): string {
     return equation;
 }
 
-function updateElementText(id: string, text: string): void {
-    const element = document.getElementById(id) as HTMLElement;
-    element.textContent = text;
+let xValues: number[] = [];
+let yValues: number[] = [];
+
+function generateData(value: string, i1: number, i2: number, step: number) {
+    for (let x = i1; x <= i2; x += step) {
+        yValues.push(eval(value));
+        xValues.push(x);
+    }
 }
 
 function drawGraph(a: number, b: number, c: number, d: number, roots: number[]): void {
     const canvas = document.getElementById("graph") as HTMLCanvasElement;
     const ctx = canvas.getContext("2d")!;
     ctx.clearRect(0, 0, canvas.width, canvas.height); // clears graph
+
+    generateData(`${a} * Math.pow(x, 3) + ${b} * Math.pow(x, 2) + ${c} * x + ${d}`, -10, 10, 0.5)
+    const config: ChartConfiguration = {
+        type: "line",
+        data: {
+            labels: xValues,
+            datasets: [{
+                fill: false,
+                pointRadius: 1,
+                borderColor: "rgba(255,0,0,0.5)",
+                data: yValues
+            }]
+        },
+        options: {
+            scales: {
+                x: {title: { display: true, text: 'x' }},
+                y: {title: { display: true, text: 'y'}
+              }
+            }   
+        }
+    };
 }
 
 function displayResults(equation: string, a: number, b: number, c: number, d: number, p: number, q: number, discriminant: number, roots: number[]): void {
     resultsContainer.style.visibility = "visible";
 
     // format roots for display (and determine if there are complex roots)
+    roots.sort((a, b) => a - b); // to sort roots by x
     const rootOne = [roots[0].toFixed(2), "0"];
     const rootTwo = (roots.length != 1) ? [roots[1].toFixed(2), "0"] : ["complex", "complex"];
     const rootThree = (roots.length != 1) ? [roots[2].toFixed(2), "0"] : ["complex", "complex"];
 
     // Get result elements
-    updateElementText("result-equation", `${equation}`);
-    updateElementText("result-p", `${p.toFixed(5)}`);
-    updateElementText("result-q", `${q.toFixed(5)}`);
-    updateElementText("result-discriminant", `${discriminant.toFixed(5)}`);
-    updateElementText("root1-x", `${rootOne[0]}`);
-    updateElementText("root1-y", `${rootOne[1]}`);
-    updateElementText("root2-x", `${rootTwo[0]}`);
-    updateElementText("root2-y", `${rootTwo[1]}`);
-    updateElementText("root3-x", `${rootThree[0]}`);
-    updateElementText("root3-y", `${rootThree[1]}`);
+    (document.getElementById("result-equation") as HTMLInputElement).value =  `${equation}`;
+    (document.getElementById("result-p") as HTMLInputElement).textContent =  `${p.toFixed(5)}`;
+    (document.getElementById("result-q") as HTMLInputElement).textContent =  `${q.toFixed(5)}`;
+    (document.getElementById("result-discriminant") as HTMLInputElement).textContent = `${discriminant.toFixed(5)}`;
+    (document.getElementById("root1-x") as HTMLInputElement).textContent =  `${rootOne[0]}`;
+    (document.getElementById("root1-y") as HTMLInputElement).textContent =  `${rootOne[1]}`;
+    (document.getElementById("root2-x") as HTMLInputElement).textContent =  `${rootTwo[0]}`;
+    (document.getElementById("root2-y") as HTMLInputElement).textContent =  `${rootTwo[1]}`;
+    (document.getElementById("root3-x") as HTMLInputElement).textContent =  `${rootThree[0]}`;
+    (document.getElementById("root3-y") as HTMLInputElement).textContent =  `${rootThree[1]}`;
     drawGraph(a, b, c, d, roots);
 }
 
@@ -104,7 +133,6 @@ form?.addEventListener("submit", (event) => {
     const d: number = Number(formData.get("d"));
 
     if (a != 0) { // a value cannot be 0
-        // Calculate key values
         const p = (3 * a * c - Math.pow(b, 2)) / (3 * a * a);
         const q = (27 * a * a * d - 9 * a * b * c + 2 * Math.pow(b, 3)) / (27 * Math.pow(a, 3));
 
@@ -119,8 +147,7 @@ form?.addEventListener("submit", (event) => {
             const root = cardanosMethod(a, b, p, q);
             displayResults(equation, a, b, c, d, p, q, discriminant, [root]);
         } else { // one real root with a double, or a triple root
-            // const rootOne = (-b + Math.sqrt(discriminant)) / (2 * a);
-            const rootOne = cardanosMethod(a, b, p, q)
+            const rootOne = cardanosMethod(a, b, p, q);
             if (p === 0 && q === 0) { // triple root
                 displayResults(equation, a, b, c, d, p, q, discriminant, [rootOne, rootOne, rootOne]);
             } else { // one real root with a double
@@ -129,7 +156,6 @@ form?.addEventListener("submit", (event) => {
             }
         }
     } else { // give an alert when a = 0
-        console.log("a cannot be 0")
         alert("Coefficient a cannot be 0. Please enter a valid cubic equation.");
     }
 })
