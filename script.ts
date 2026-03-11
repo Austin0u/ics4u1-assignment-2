8ae6427ac85081d0c088ca4cebd954727e98ae41
Austin <austin.ou@student.tdsb.on.ca>
2026-03-11 07:31:26 -0400

    Optimized css, fixed HTML, optimized and styled script functionality (changed roots to a point obj., semicolons and types)

--- script.ts
+++ script.ts
@@ -1,4 +1,5 @@
 const form = document.getElementById("cubic-form") as HTMLFormElement;
+type Point = { x: string, y: string }; // use objects for cleaner "x" and "y" instead of array indexing
 
 // Displaying results
 function getCubicEquation(a: number, b: number, c: number, d: number): string {
@@ -33,26 +34,26 @@ function getCubicEquation(a: number, b: number, c: number, d: number): string {
         equation += sign + Math.abs(d);
     }
 
-    return (equation += " = 0");
+    return (equation + " = 0");
 }
 
 function drawGraph(a: number, b: number, c: number, d: number, roots: number[]): void {
     const canvas = document.getElementById("graph") as HTMLCanvasElement;
     const ctx = canvas.getContext("2d")!;
-    const WIDTH = ctx.canvas.width;
-    const HEIGHT = ctx.canvas.height;
-    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+    const width = ctx.canvas.width;
+    const height = ctx.canvas.height;
+    ctx.clearRect(0, 0, width, height);
 
     const gridSize: number = 20; // scaling (e.g. 10 = 5 units left and right, going from -5 to 5). should be an even number
 
     // convert graph coords to canvas (aka translating cartesian coordinates)
     const toCanvasX = (x: number): number => {
-        return WIDTH / 2 + (x / gridSize) * WIDTH; //ex. x = 5, -> (5 + 10) / 20 becomes the % on the graph, then times width to scale it
-    }
+        return width / 2 + (x / gridSize) * width; //ex. x = 5, -> (5 + 10) / 20 becomes the % on the graph, then times width to scale it
+    };
 
     const toCanvasY = (y: number): number => {
-        return HEIGHT / 2 - (y / gridSize) * HEIGHT; // canvas y starts at top, so move to middle and subtract % of graph
-    }
+        return height / 2 - (y / gridSize) * height; // canvas y starts at top, so move to middle and subtract % of graph
+    };
 
     // Draw grid
     ctx.strokeStyle = "#e0e0e0";
@@ -62,7 +63,7 @@ function drawGraph(a: number, b: number, c: number, d: number, roots: number[]):
         const cx: number = toCanvasX(x);
         ctx.beginPath();
         ctx.moveTo(cx, 0);
-        ctx.lineTo(cx, HEIGHT);
+        ctx.lineTo(cx, height);
         ctx.stroke();
     }
 
@@ -70,7 +71,7 @@ function drawGraph(a: number, b: number, c: number, d: number, roots: number[]):
         const cy = toCanvasY(y);
         ctx.beginPath();
         ctx.moveTo(0, cy);
-        ctx.lineTo(WIDTH, cy);
+        ctx.lineTo(width, cy);
         ctx.stroke();
     }
 
@@ -78,8 +79,8 @@ function drawGraph(a: number, b: number, c: number, d: number, roots: number[]):
     ctx.strokeStyle = "#aaaaaa";
     ctx.lineWidth = 1;
     ctx.beginPath();
-    ctx.moveTo(0, HEIGHT / 2); ctx.lineTo(WIDTH, HEIGHT / 2); // x axis
-    ctx.moveTo(WIDTH / 2, 0); ctx.lineTo(WIDTH / 2, HEIGHT); // y axis
+    ctx.moveTo(0, height / 2); ctx.lineTo(width, height / 2); // x axis
+    ctx.moveTo(width / 2, 0); ctx.lineTo(width / 2, height); // y axis
     ctx.stroke();
 
     // Draw curve
@@ -90,7 +91,7 @@ function drawGraph(a: number, b: number, c: number, d: number, roots: number[]):
         const cx = toCanvasX(x);
         const cy = toCanvasY(a * Math.pow(x, 3) + b * Math.pow(x, 2) + c * x + d); // use abcd values to calculate y for each x
 
-        ctx.lineTo(cx, cy)
+        ctx.lineTo(cx, cy);
     }
     ctx.stroke();
 
@@ -110,21 +111,25 @@ function displayResults(equation: string, a: number, b: number, c: number, d: nu
 
     // format roots for display (and determine if there are complex roots)
     roots.sort((a, b) => a - b); // to sort roots by x
-    const rootOne: string[] = [roots[0].toFixed(2), "0"];
-    const rootTwo: string[] = [(roots.length != 1) ? roots[1].toFixed(2) : "complex", "0"];
-    const rootThree: string[] = [(roots.length != 1) ? roots[2].toFixed(2) : "complex", "0"];
+    const rootOne: Point = {x: roots[0].toFixed(2), y: "0"}; 
+    const rootTwo: Point = {x: (roots.length !== 1) ? roots[1].toFixed(2) : "complex", y: "0"};
+    const rootThree: Point = {x: (roots.length !== 1) ? roots[2].toFixed(2) : "complex", y: "0"};
+   
+    // Updates text content of result elements
+    const updateElement = (id: string, value: string): void => {
+        (document.getElementById(id) as HTMLElement).textContent = value;
+    };
 
-    // Get result elements
-    (document.getElementById("result-equation") as HTMLInputElement).textContent = `${equation}`;
-    (document.getElementById("result-p") as HTMLInputElement).textContent = `${p.toFixed(5)}`;
-    (document.getElementById("result-q") as HTMLInputElement).textContent = `${q.toFixed(5)}`;
-    (document.getElementById("result-discriminant") as HTMLInputElement).textContent = `${discriminant.toFixed(5)}`;
-    (document.getElementById("root1-x") as HTMLInputElement).textContent = `${rootOne[0]}`;
-    (document.getElementById("root1-y") as HTMLInputElement).textContent = `${rootOne[1]}`;
-    (document.getElementById("root2-x") as HTMLInputElement).textContent = `${rootTwo[0]}`;
-    (document.getElementById("root2-y") as HTMLInputElement).textContent = `${rootTwo[1]}`;
-    (document.getElementById("root3-x") as HTMLInputElement).textContent = `${rootThree[0]}`;
-    (document.getElementById("root3-y") as HTMLInputElement).textContent = `${rootThree[1]}`;
+    updateElement("result-equation", equation);
+    updateElement("result-p", p.toFixed(5));
+    updateElement("result-q", q.toFixed(5));
+    updateElement("result-discriminant", discriminant.toFixed(5));
+    updateElement("root1-x", rootOne.x);
+    updateElement("root1-y", rootOne.y);
+    updateElement("root2-x", rootTwo.x);
+    updateElement("root2-y", rootTwo.y);
+    updateElement("root3-x", rootThree.x);
+    updateElement("root3-y", rootThree.y);
     drawGraph(a, b, c, d, roots);
 }
 
@@ -134,17 +139,13 @@ function trigonometricMethod(a: number, b: number, p: number, q: number): number
 
     const calcRoot = (angle: number): number => {
         return 2 * Math.sqrt(-p / 3) * Math.cos(angle) - b / (3 * a);
-    }
+    };
 
-    const rootOne: number = calcRoot(theta);
-    const rootTwo: number = calcRoot(theta + 2 * Math.PI / 3);
-    const rootThree: number = calcRoot(theta + 4 * Math.PI / 3);
-
-    return [rootOne, rootTwo, rootThree];
+    return [calcRoot(theta), calcRoot(theta + 2 * Math.PI / 3), calcRoot(theta + 4 * Math.PI / 3)];
 }
 
 function cardanosMethod(a: number, b: number, p: number, q: number): number {
-    return Math.cbrt((-q / 2) + Math.sqrt(Math.pow(q / 2, 2) + Math.pow(p / 3, 3))) + Math.cbrt((-q / 2) - Math.sqrt(Math.pow(q / 2, 2) + Math.pow(p / 3, 3))) - b / (3 * a)
+    return Math.cbrt((-q / 2) + Math.sqrt(Math.pow(q / 2, 2) + Math.pow(p / 3, 3))) + Math.cbrt((-q / 2) - Math.sqrt(Math.pow(q / 2, 2) + Math.pow(p / 3, 3))) - b / (3 * a);
 }
 
 // Form submission
@@ -158,7 +159,7 @@ form?.addEventListener("submit", (event) => {
     const c: number = Number(formData.get("c"));
     const d: number = Number(formData.get("d"));
 
-    if (a != 0) { // a value cannot be 0
+    if (a !== 0) { // a value cannot be 0
         const p: number = (3 * a * c - Math.pow(b, 2)) / (3 * a * a);
         const q: number = (27 * a * a * d - 9 * a * b * c + 2 * Math.pow(b, 3)) / (27 * Math.pow(a, 3));
         const discriminant: number = (q / 2) * (q / 2) + (p / 3) * (p / 3) * (p / 3); // Math.pow() causes some issues in some cases
@@ -167,7 +168,7 @@ form?.addEventListener("submit", (event) => {
         // Root cases
         if (discriminant < 0) { // three distinct roots 
             const roots: number[] = trigonometricMethod(a, b, p, q);
-            displayResults(equation, a, b, c, d, p, q, discriminant, [roots[0], roots[1], roots[2]]);
+            displayResults(equation, a, b, c, d, p, q, discriminant, roots);
         } else if (discriminant > 0) { // one real root and two complex roots
             const root: number = cardanosMethod(a, b, p, q);
             displayResults(equation, a, b, c, d, p, q, discriminant, [root]);
@@ -183,4 +184,4 @@ form?.addEventListener("submit", (event) => {
     } else { // give an alert when a = 0
         alert("Coefficient a cannot be 0");
     }
-})
+});
