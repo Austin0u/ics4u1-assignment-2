75de30f314268e64f41378d26d8bca9733661658
Ou <342906989@tdsb.ca>
2026-03-12 09:47:24 -0400

    Assignment graded (formatting fix)

--- script.ts
+++ script.ts
@@ -86,16 +86,19 @@ function drawGraph(a: number, b: number, c: number, d: number, roots: number[]):
     ctx.strokeStyle = "#E49273";
     ctx.lineWidth = 2;
     ctx.beginPath();
+
     for (let x = -gridSize / 2; x < gridSize / 2; x += 0.15) { // smaller step = more detailed curve.
         const cx = toCanvasX(x);
         const cy = toCanvasY(a * Math.pow(x, 3) + b * Math.pow(x, 2) + c * x + d); // use abcd values to calculate y for each x
 
         ctx.lineTo(cx, cy);
     }
+
     ctx.stroke();
 
     // Draw roots as dots
     ctx.fillStyle = "#7180acff";
+    
     for (const root of roots) {
         const cx = toCanvasX(root);
         const cy = toCanvasY(0);
